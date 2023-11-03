cc015286f6f13a3703c0e34b027dabc432b764d6
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-03 19:55:03 +0100

    done

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 const PORT = 3000
 
-app.use(__dirname, '/public', express.static())
+app.use(__dirname + '/public', express.static())
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
