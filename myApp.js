78d57a12e50e90e5222f79e65de1eb22a476aa32
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-03 20:40:22 +0100

    done

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 const path = require('path')
-// const PORT = 3000
+const message = process.env.MESSAGE_STYLE
+console.log(message)
 
 // Mount Middleware for static files
 app.use("/public", express.static(path.join(__dirname, 'public')))
@@ -13,14 +14,10 @@ app.get("/", (req, res) => {
 })
 
 app.get("/json", (req, res) => {
-  if(process.env.MESSAGE_STYLE=== "uppercase") {
+  if(message=== "uppercase") {
     res.json({"message": "HELLO JSON"})
   } else {
     res.json({"message": "Hello json"})
   }
 })
-
-// app.listen(PORT, ()=> {
-//   console.log(`Listening on port ${PORT}`)
-// })
  module.exports = app;
